4726959f5479678d3039a6d59cc74aafdefe7943
Bojan Terzic <bojan.terzic@symphony.is>
2022-09-12 10:59:59 +0200

    Expanded checks for single elements (author username) and  multiple elements (article tags)

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,43 +1,37 @@
 /// <reference types="Cypress" />
 
-import { faker } from '@faker-js/faker';
-import { signUpPage } from '../pages/signup';
-import { settingsPage } from '../pages/settings';
-import { articleCreatePage } from '../pages/createarticle';
-import { articlePage } from '../pages/article';
+import { faker } from "@faker-js/faker";
+import { signUpPage } from "../pages/signup";
+import { settingsPage } from "../pages/settings";
+import { articleCreatePage } from "../pages/createarticle";
+import { articlePage } from "../pages/article";
 
-describe('Cypress Demo Website Automation Scenarios', () => {
-
+describe("Cypress Demo Website Automation Scenarios", () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit("/");
   });
 
-  it('Check if homepage loads successfully', () => {
-
+  it("Check if homepage loads successfully", () => {
     // Check if navbar is present
-    cy.get('.navbar')
-      .should('be.visible')
-      .should('contain', 'Home')
-      .should('contain', 'Sign in')
-      .should('contain', 'Sign up')
+    cy.get(".navbar")
+      .should("be.visible")
+      .should("contain", "Home")
+      .should("contain", "Sign in")
+      .should("contain", "Sign up");
 
-    cy.get('.banner').within(() => {
+    cy.get(".banner").within(() => {
       // Check if logo(header title) is present
-      cy.contains('.logo-font', 'conduit').should('be.visible')
+      cy.contains(".logo-font", "conduit").should("be.visible");
 
       // Check if header subtitle is present
-      cy.contains('p', 'A place to share your knowledge.').should('be.visible')
-    })
+      cy.contains("p", "A place to share your knowledge.").should("be.visible");
+    });
 
     // Check if articles are displayed
-    cy.get('article-preview')
-      .should('be.visible')
-      .should('have.length', 3)
+    cy.get("article-preview").should("be.visible").should("have.length", 3);
+  });
 
-  })
-
-  it('Check if User can modify profile successfully', () => {
-
+  it("Check if User can modify profile successfully", () => {
     // Click on Sign up link
     signUpPage.navigate();
 
@@ -49,11 +43,11 @@ describe('Cypress Demo Website Automation Scenarios', () => {
     // Click Sign up
     signUpPage.clickSignUpButton();
 
-    // Click Settings 
+    // Click Settings
     settingsPage.navigate();
 
     // Enter bio
-    const profileShortBio = 'Cypress-entered short bio';
+    const profileShortBio = "Cypress-entered short bio";
     settingsPage.typeInShortBio(profileShortBio);
 
     // Set Profile Picture
@@ -64,86 +58,104 @@ describe('Cypress Demo Website Automation Scenarios', () => {
     settingsPage.clickOnUpdateSettings();
 
     // Check if bio is set
-    cy.get('p[ng-bind="::$ctrl.profile.bio"]').should('have.text', profileShortBio)
+    cy.get('p[ng-bind="::$ctrl.profile.bio"]').should(
+      "have.text",
+      profileShortBio
+    );
 
     // Check if Profile Picture is set
-    cy.get('.user-img')
-      .invoke('attr', 'src')
-      .should('eq', profilePictureUrl)
-
+    cy.get(".user-img").invoke("attr", "src").should("eq", profilePictureUrl);
   });
 
-  it('Check if User can submit an Article', () => {
-
+  it("Check if User can submit an Article", () => {
     // Login using custom command
     cy.demoLogin(
-      Cypress.env('existingUsername'),
-      Cypress.env('existingPassword'))
+      Cypress.env("existingUsername"),
+      Cypress.env("existingPassword")
+    );
 
     // Click on New Article link
     articleCreatePage.navigate();
 
     // Fill in Article details with data from previously created fixture
-    cy.fixture('article').then((articleData) => {
+    cy.fixture("article").then((articleData) => {
       articleCreatePage.typeInArticleTitle(articleData.title);
       articleCreatePage.typeInArticleDescription(articleData.description);
       articleCreatePage.typeInArticleBody(articleData.body);
       articleCreatePage.typeInArticleTag(articleData.tag);
-    })
+    });
 
     // Click on Publish Article
     articleCreatePage.clickOnPublishArticle();
 
     // Check if article data is valid
-    cy.fixture('article').then((articleData) => {
-      articlePage.checkIfArticleTitleContains(articleData.title)
-      articlePage.checkIfArticleBodyContains(articleData.body)
-      articlePage.checkIfArticleTagsContain(articleData.tag)
-    })
+    cy.fixture("article").then((articleData) => {
+      articlePage.checkIfArticleTitleContains(articleData.title);
+      articlePage.checkIfArticleBodyContains(articleData.body);
+      articlePage.checkIfArticleTagsContain(articleData.tag);
+    });
 
     // Clean up - delete article after successful test
     articlePage.clickDeleteArticleButton();
-
   });
 
-  it('Check if display of mocked articles is correct', () => {
-
+  it("Check if display of mocked articles is correct", () => {
     // Intercept
-    cy.fixture('articles').then((articlesData) => {
-      cy.intercept("GET", "feed*", articlesData)
-    })
+    cy.fixture("articles").then((articlesData) => {
+      cy.intercept("GET", "feed*", articlesData);
+    });
 
     // Login - this time we use an API method to bypass UI login
     cy.loginUsingApi(
-      Cypress.env('existingUsername'),
-      Cypress.env('existingPassword'));
+      Cypress.env("existingUsername"),
+      Cypress.env("existingPassword")
+    );
 
-    cy.visit('/')
+    cy.visit("/");
 
     // Check if articles have correct values
-    cy.fixture('articles').then((articlesData) => {
-
+    cy.fixture("articles").then((articlesData) => {
       // Check first article
-      cy.get('article-preview').eq(0).within(() => {
-        cy.get('h1').should('contain', articlesData.articles[0].title)
-        cy.get('p').should('contain', articlesData.articles[0].description)
-        cy.get('li').should('contain', articlesData.articles[0].tagList[0])
-      })
+      cy.get("article-preview")
+        .eq(0)
+        .within(() => {
+          cy.get("h1").should("contain", articlesData.articles[0].title);
+          cy.get("p").should("contain", articlesData.articles[0].description);
+          cy.get("li").should("contain", articlesData.articles[0].tagList[0]);
+        });
 
       // Check second article
-      cy.get('article-preview').eq(1).within(() => {
-        cy.get('h1').should('contain', articlesData.articles[1].title)
-        cy.get('p').should('contain', articlesData.articles[1].description)
-        cy.get('li').should('contain', articlesData.articles[1].tagList[0])
-      })
+      cy.get("article-preview")
+        .eq(1)
+        .within(() => {
+          cy.get("h1").should("contain", articlesData.articles[1].title);
+          cy.get("p").should("contain", articlesData.articles[1].description);
+          cy.get("li").should("contain", articlesData.articles[1].tagList[0]);
+        });
 
       // Check third article
-      cy.get('article-preview').eq(2).within(() => {
-        cy.get('h1').should('contain', articlesData.articles[2].title)
-        cy.get('p').should('contain', articlesData.articles[2].description)
-        cy.get('li').should('contain', articlesData.articles[2].tagList[0])
-      })
-    })
+      cy.get("article-preview")
+        .eq(2)
+        .within(() => {
+          cy.get("h1").should("contain", articlesData.articles[2].title);
+          cy.get("p").should("contain", articlesData.articles[2].description);
+          // Checking for multiple tags
+          cy.get("li").then(($ele) => {
+            let tagList = [];
 
+            const tags = Array.from($ele, (el) => el.innerText);
+            tags.forEach((text) => tagList.push(text));
+
+            expect(tagList).to.be.eql(articlesData.articles[2].tagList);
+          });
+          // Check for author username
+          cy.get('a[class="author ng-binding"]').then(($author) => {
+            let authorText = $author.text();
+            expect(authorText).to.be.eql(
+              articlesData.articles[2].author.username
+            );
+          });
+        });
+    });
   });
-})
+});
